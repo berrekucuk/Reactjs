d4c7fa4e5e827395838645bd09c4e0e6effff930
berrekucuk <berrekucuk@hotmail.com>
2024-07-11 15:19:31 +0300

    02.07.2024 Props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import SiteHeader from "./components/SiteHeader";
 import SiteContent from "./components/SiteContent";
 import SiteFooter from "./components/SiteFooter";
-import { Route,Routes } from "react-router-dom";
+import {Link,Route,Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import NotFound from "./pages/NotFound";
+import ProductsPage from "./pages/ProductsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 
 
 function App(){
   return <>
+  <ul style={{display:'flex', justifyContent:'space-evenly'}}>
+    <Link to ='/'>Home</Link>
+    <Link to ='/about'>About</Link>
+    <Link to ='/contact'>Contact</Link>
+    <Link to ='/products'>Products</Link>
+    <Link to ='/profile'>Profile</Link>
+
+  </ul>
+
+
     <Routes>
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/about" element={<AboutPage/>}/>
         <Route path="/contact" element={<ContactPage/>}/>
+        <Route path="/products" element={<ProductsPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
         <Route path="*" element={<NotFound/>}/>
     </Routes>
     
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Detail from '../components/Detail'
+
+function ProfilePage() {
+
+    let user = {
+        name: "John Doe",
+        email: "john@mai.com",
+        phone: "5554443322"
+    }
+
+    let points = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+
+  return <>
+    <h1>Profile Detail</h1>
+    <Detail name={user.name} email={user.email} phone={user.phone} points={points} />
+  </>
+}
+
+export default ProfilePage
